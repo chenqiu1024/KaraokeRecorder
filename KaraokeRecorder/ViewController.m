8e2d1e342da3b396032e6cf0b2118124ac391c69
chenqiu1024 <godspeed1024@gmail.com>
2019-05-27 15:34:25 +0800

    Start/Stop Recording/Playing buttons;

--- KaraokeRecorder/ViewController.m
+++ KaraokeRecorder/ViewController.m
@@ -13,6 +13,13 @@
 {
     AudioUnitManager* _auMgr;
 }
+
+//@property (nonatomic, strong) IBOutlet UIButton* recordButton;
+//@property (nonatomic, strong) IBOutlet UIButton* playButton;
+
+-(IBAction)onRecordButtonPressed:(id)sender;
+-(IBAction)onPlayButtonPressed:(id)sender;
+
 @end
 
 @implementation ViewController
@@ -21,8 +28,38 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     _auMgr = [[AudioUnitManager alloc] init];
-    [_auMgr startPlaying];
 }
 
+-(IBAction)onRecordButtonPressed:(id)sender {
+    UIButton* button = (UIButton*) sender;
+    if (0 == button.tag)
+    {
+//        [_auMgr startPlaying];
+        button.tag = 1;
+        [button setTitle:@"Stop Recording" forState:UIControlStateNormal];
+    }
+    else if (1 == button.tag)
+    {
+//        [_auMgr stopPlaying];
+        button.tag = 0;
+        [button setTitle:@"Start Recording" forState:UIControlStateNormal];
+    }
+}
+
+-(IBAction)onPlayButtonPressed:(id)sender {
+    UIButton* button = (UIButton*) sender;
+    if (0 == button.tag)
+    {
+        [_auMgr startPlaying];
+        button.tag = 1;
+        [button setTitle:@"Stop Playing" forState:UIControlStateNormal];
+    }
+    else if (1 == button.tag)
+    {
+        [_auMgr stopPlaying];
+        button.tag = 0;
+        [button setTitle:@"Start Playing" forState:UIControlStateNormal];
+    }
+}
 
 @end
